5807048150ff7439c9981bcb46d86139afc32a3e
Raphael Luba <raphael@leanbyte.com>
2023-02-06 11:45:28 +0100

    Write the submit result to the last column

--- signup.js
+++ signup.js
@@ -1,71 +1,82 @@
 function on_form_submitted(e) {
-    Logger.log(`Event data: ${JSON.stringify(e.namedValues)}`);
+    Logger.log(`Event data: ${JSON.stringify(e.namedValues)}, event: ${JSON.stringify(e)}`);
+    const range = e.range;
+    const statusRange = range.offset(0, range.getWidth(), 1, 1);
+    try {
+        const submitted_data = e.namedValues;
 
-    const submitted_data = e.namedValues;
+        const package_map = get_configured_packages();
+        const field_map = get_field_map();
 
-    const package_map = get_configured_packages();
-    const field_map = get_field_map();
+        const api_key = get_api_key();
+        const me = fetch_me(api_key);
+        Logger.log(me);
 
-    const api_key = get_api_key();
-    const me = fetch_me(api_key);
-    Logger.log(me);
+        const spaces = fetch(api_key, '/spaces');
 
-    const spaces = fetch(api_key, '/spaces');
-
-    let member_data = {
-        account: me.account,
-        notes: `Added via "Fabman Self Sign-Up for Google Sheets & Forms"`,
-    };
-    let package_ids = [];
-
-    // Retrieve original order of fields in the form and sort the field names accordingly
-    const field_names = Object.keys(submitted_data);
-    const form_items = get_form().getItems();
-    const ordered_titles = form_items.map(i => i.getTitle());
-    field_names.sort((a, b) => ordered_titles.indexOf(a) - ordered_titles.indexOf(b));
-
-    for (const field of field_names) {
-        set_value(field, submitted_data[field], field_map, package_map, member_data, package_ids);
-    }
-
-    if (!(member_data.firstName || member_data.lastName)) {
-        throw new Error("A member must have at least a first name or a last name");
-    }
-
-    let member_space;
-    if (member_data.space) {
-        member_space = spaces.find(s => s.id == member_data.space);
-    } else {
-        if (spaces.length > 1) {
-            throw new Error(`Account ${me.id} contains ${spaces.length} spaces, so you need to specify one.`);
-        }
-        member_data.space = spaces[0].id;
-        member_space = spaces[0];
-    }
-
-    const member_response = try_send_request(api_key, 'POST', '/members', member_data);
-    if (member_response.getResponseCode() > 299) {
-        if (is_error(member_response, 422, 'duplicateEmailAddress')) {
-            // @ToDo: Better email template?
-            GmailApp.sendEmail(member_data.emailAddress, `Sign-up for ${member_space.name}`, `You tried signing up for ${member_space.name}, but there’s already a member with your email address.\n\n* If you already have an account and want to sign in, please go to https://fabman.io/members/${member_data.account}/login\n* If you have forgotten your password, then go to https://fabman.io/user/password-forgotten`);
-            return;
-        } else {
-            return handle_request_error(member_response);
-        }
-    }
-
-    const member = JSON.parse(member_response.getContentText());
-
-    for (const package_id of package_ids) {
-        const member_package = {
-            package: package_id,
-            fromDate: Utilities.formatDate(new Date(), member_space.timezone || "UTC", "yyyy-MM-dd"),
-            notes: `Assigned during self sign-up`,
+        let member_data = {
+            account: me.account,
+            notes: `Added via "Fabman Self Sign-Up for Google Sheets & Forms"`,
         };
-        send_request(api_key, 'POST', `/members/${member.id}/packages`, member_package);
-    }
+        let package_ids = [];
 
-    // @ToDo: Write "added to Fabman" + a link into the column next to the member or write "failed" + error details if it failed
+        // Retrieve original order of fields in the form and sort the field names accordingly
+        const field_names = Object.keys(submitted_data);
+        const form_items = get_form().getItems();
+        const ordered_titles = form_items.map(i => i.getTitle());
+        field_names.sort((a, b) => ordered_titles.indexOf(a) - ordered_titles.indexOf(b));
+
+        for (const field of field_names) {
+            set_value(field, submitted_data[field], field_map, package_map, member_data, package_ids);
+        }
+
+        if (!(member_data.firstName || member_data.lastName)) {
+            throw new Error("A member must have at least a first name or a last name");
+        }
+
+        let member_space;
+        if (member_data.space) {
+            member_space = spaces.find(s => s.id == member_data.space);
+        } else {
+            if (spaces.length > 1) {
+                throw new Error(`Account ${me.id} contains ${spaces.length} spaces, so you need to specify one.`);
+            }
+            member_data.space = spaces[0].id;
+            member_space = spaces[0];
+        }
+
+        const member_response = try_send_request(api_key, 'POST', '/members', member_data);
+        if (member_response.getResponseCode() > 299) {
+            if (is_error(member_response, 422, 'duplicateEmailAddress')) {
+                // @ToDo: Better email template?
+                GmailApp.sendEmail(member_data.emailAddress, `Sign-up for ${member_space.name}`, `You tried signing up for ${member_space.name}, but there’s already a member with your email address.\n\n* If you already have an account and want to sign in, please go to https://fabman.io/members/${member_data.account}/login\n* If you have forgotten your password, then go to https://fabman.io/user/password-forgotten`);
+                return;
+            } else {
+                handle_request_error(member_response);
+                return;
+            }
+        }
+
+        const member = JSON.parse(member_response.getContentText());
+
+        for (const package_id of package_ids) {
+            const member_package = {
+                package: package_id,
+                fromDate: Utilities.formatDate(new Date(), member_space.timezone || "UTC", "yyyy-MM-dd"),
+                notes: `Assigned during self sign-up`,
+            };
+            send_request(api_key, 'POST', `/members/${member.id}/packages`, member_package);
+        }
+
+        const resultValue = SpreadsheetApp.newRichTextValue()
+            .setText('Added to Fabman')
+            .setLinkUrl(`https://fabman.io/manage/${member.account}/members/${member.id}`)
+            .build();
+        statusRange.setRichTextValue(resultValue);
+    } catch (e) {
+        statusRange.setValue(`Error occurred while trying to create the member:\n${e.toString()}`);
+        throw e;
+    }
 }
 
 function set_value(form_field_name, form_value, field_map, package_map, member_data, package_ids) {
